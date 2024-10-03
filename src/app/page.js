64cfe91556b8dccfbec0fda9b854fe33454d7fb3
jonathan-at-230349630805 <jonathancosta1717@gmail.com>
2024-10-03 14:02:14 -0300

    update fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
+"use client";
 import Image from "next/image";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./page.module.css";
-import Login from "@/components/Login/Index";
+
 import React, { useEffect, useState } from "react";
 import LoadingSpinner from '@/components/Loading';
-import LoginForm from '@/components/Login/Index';
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-	  const timer = setTimeout(() => setLoading(false), 3000);
+	  const timer = setTimeout(() => 3000);
 	  return () => {
       clearTimeout(timer)
+      window.location.href = "Login"
     };
 	}, []);
   
@@ -20,7 +21,7 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      {loading? <LoadingSpinner /> : <LoginForm />}
+      <LoadingSpinner /> 
     </main>
   );
 }
