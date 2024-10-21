d49d22b197b1b29687edd9c7a2a6ff3c14e6b400
jonathan-at-230349630805 <jonathancosta1717@gmail.com>
2024-10-21 14:23:08 -0300

    hotFix loading infinito

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./page.module.css";
 
@@ -7,21 +6,20 @@ import React, { useEffect, useState } from "react";
 import LoadingSpinner from '@/components/Loading';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-	  const timer = setTimeout(() => 3000);
-	  return () => {
+  useEffect(() => {
+    const timer = setTimeout(() => (null), 100);
+    return () => {
       clearTimeout(timer)
       window.location.href = "Login"
     };
-	}, []);
-  
+  }, []);
+
 
 
   return (
     <main className={styles.main}>
-      <LoadingSpinner /> 
+      <LoadingSpinner />
     </main>
   );
 }
